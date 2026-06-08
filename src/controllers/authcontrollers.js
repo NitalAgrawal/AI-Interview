2d3a286444fb4eee7a7d544c83a1e8906535eb2e
NitalAgrawal <nitalagrawal5@gmail.com>
2026-06-08 18:02:05 +0530

    logout

--- src/controllers/authcontrollers.js
+++ src/controllers/authcontrollers.js
@@ -1,7 +1,8 @@
 const userModel= require("../models/usermodel")
 const authRouter = require("../routes/authroutes")
-
-
+const bcrypt = require("bcryptjs")
+const jwt=require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
 /*
 * @name registerusercontroller
 * @description register a new user,expects username, email, password
@@ -9,7 +10,7 @@ const authRouter = require("../routes/authroutes")
 */
 
 async function registerUserController(req,res) {
-    const{ username, email, password}
+    const{ username, email, password} = req.body;
        if(!username || !email || !password){
           return res.status(400).json({
              message :"please provide usernmae,email,password"
@@ -19,8 +20,95 @@ async function registerUserController(req,res) {
        const isUserAlreadyExists = await userModel.findOne({
          $or: [ { username },{ email }]
        })
+
+       if(isUserAlreadyExists){
+         return res.status(400).json({
+            message:"account already exists with this email address or username "
+         })
+       }
+
+       const hash= await bcrypt.hash(password, 10)
+
+       const user= await userModel.create({
+         username,
+         email,
+         password: hash
+       })
+
+       const token = jwt.sign(
+         { id: user._id, username: user.username },
+         process.env.JWT_SECRET,
+         { expiresIn: "1d" }
+       )
+
+       res.cookie("token",token)
+
+       res.status(201).json({
+         message:"user registered successfully",
+         user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+         }
+       })
 }
 
+/*
+* @name loginusercontroller
+* @description login a  user,expects  email and password in the request body 
+ * @access Public
+*/
+async function loginUserController(req,res) {
+   const { email, password } = req.body
+
+   // whather the account with the particular email exists or not
+   const user = await userModel.findOne({ email })
+
+   if(!user) {
+      return res.status(400).json({
+         message:" Invalid email or password"
+      })
+   }
+
+   const isPasswordValid=await bcrypt.compare(password, user.password)
+
+   if(!isPasswordValid){
+      return res.status(400).json({
+         message:" Invalid email or password"
+      })
+   }
+
+   // password valid niklta hai to now will move on towards the token
+   const token = jwt.sign(
+         { id: user._id, username: user.username },
+         process.env.JWT_SECRET,
+         { expiresIn: "1d" }
+       )
+
+       res.cookie("token",token)
+       res.status(200).json({
+         message:"user loggedIn successfully",
+         user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+         }
+       })
+}
+
+async function logoutUserController(req,res){
+   const token = req.cookies.token
+
+   if(token){
+      await tokenBlacklistModel.create({ token })
+   }
+   res.clearCookie("token")
+   res.status(200).json({
+      message:"user loged out successfully"
+   })
+}
 module.exports={
-    registerUserController
+    registerUserController,
+    loginUserController,
+    logoutUserController
 }
--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -3,10 +3,23 @@ const {Router}=require("express")
 const authController = require("../controllers/authcontrollers")
 const authRouter = Router();
 
-/*
- @route POST /api/auth/register
- @description register a new user
- @access Public
+/**
+ *@route POST /api/auth/register
+ *@description register a new user
+ *@access Public
 */
 authRouter.post("/register",authController.registerUserController)
-module.exports = authRouter
+
+/**
+* @route POST /api/auth/login
+ *@description login a  user with email and password
+ *@access Public
+*/authRouter.post("/login",authController.loginUserController)
+
+/** 
+ * @route GET /api/auth/login
+ *@description clear token from user cookie and add the token in blaclist 
+ *@access Public
+*/
+authRouter.get("/logout",authController.logoutUserController)
+module.exports = authRouter              
