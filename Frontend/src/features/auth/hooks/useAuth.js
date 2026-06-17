8566a6df4ddb0e0b56f2ee1d1e76b38e588cfcbd
NitalAgrawal <nitalagrawal5@gmail.com>
2026-06-17 10:57:36 +0530

    hook1

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,9 +1,17 @@
 import { useContext } from "react";
 // Context ke andar stored data ko access karna.
+
 import { AuthContext } from "../auth.context";
 import { login, register, logout, getMe } from "../services/auth.api";
 
 export const useuth = () => {
     const context =  useContext(AuthContext)
-    const { user, setUser, loading, setLoading}
+    const { user, setUser, loading, setLoading}= context
+
+    const handleLogin = async ({ email, password}) => {
+         setLoading(true)
+         const data = await login({ email,password })
+         setUser(data.user)
+         setLoading(false)
+    }
 }
