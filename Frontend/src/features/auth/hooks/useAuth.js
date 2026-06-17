ce3fff138ec11773e4cd98032b29813bd733cd73
NamanB128 <namanbansal128@gmail.com>
2026-06-17 10:43:29 +0530

    state

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+// Context ke andar stored data ko access karna.
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+export const useuth = () => {
+    const context =  useContext(AuthContext)
+    const { user, setUser, loading, setLoading}
+}
