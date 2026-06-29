7d6746c4184edaf8b9c4cb5e0c99fe21f056f837
NitalAgrawal <nitalagrawal5@gmail.com>
2026-06-29 23:35:56 +0530

    interview report generation

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -1,7 +1,17 @@
+const pdfParse = require("pdf-parse")
+const generateInterViewReport = require("../services/ai.service")
+
 async function generateInterViewReportController(req, res){
+    const resumefile = req.file
 
+    const resumeContent = pdfParse(req.file.buffer)
+    const { selfDescription, jobDescription} = req.body
 
-
+    const interViewReportByAi = await generateInterViewReport({
+        resume: resumeContent,
+        selfDescription,
+        jobDescription
+    })
 }
 
 
