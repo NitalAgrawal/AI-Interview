810345a72687ef83293e459ce45ae29d64a9efeb
NitalAgrawal <nitalagrawal5@gmail.com>
2026-07-12 23:33:56 +0530

    rwsume upload

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -8,13 +8,13 @@ const api = axios.create({
 /**
  * @description service to generate interview report based on user self description , resume and job Description
  */
-export const generateInterViewReport = ({ resumeFile, selfDescription, jobDescription }) => {
+export const generateInterViewReport = async ({ resumeFile, selfDescription, jobDescription }) => {
     const formData = new FormData();// frontend se backend ko file bhejna is done through the form data 
-    formData.append("resumeFile", resumeFile);
+    formData.append("resume", resumeFile);
     formData.append("selfDescription", selfDescription);
     formData.append("jobDescription", jobDescription);
 
-    const response = api.post("/api/interview", formData, {
+    const response = await api.post("/api/interview", formData, {
         headers: {
             "Content-Type": "multipart/form-data"
         }
